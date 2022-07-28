9bc0afbd348630e365164fa341dd9dd2e7285ce0
dadajonovfarhod <dadajonovfarhod5@gmail.com>
2022-07-28 11:55:14 +0500

    my first javascript project

---
+++ script.js
@@ -0,0 +1,35 @@
+
+var firstName = prompt("Ismingizni kiriting"),
+ lastName = prompt ("Familyangizni kiriting"),
+ birthDate = prompt ("Tug'ilgan kuningizni kiriting"),
+ email= prompt (" Foydalanuvchining email pochtasi"),
+ address= prompt ( "Foydalanuvchining manzili"),
+ userNum = prompt("Foydalanuvchining nomeri"),
+ workPlace = prompt ("Foydalanuvchining ish joyi"),
+ profession =prompt ("Foydalanuvchining kasbi"),
+ hobby =prompt ("Foydalanuvchining sevimli mashg'uloti"),
+ car = prompt("Foydalanuvchining  moshinasi"),
+ text = alert ("Malumotiz uchun rahmat");
+
+console.log(
+  
+`Foydalanuvchining ismi ${firstName}
+
+ Foydalanuvchining familyasi ${lastName} 
+ 
+ Foydalanuvchining tug'ilgan yili ${birthDate} 
+ 
+ Foydalanuvchining email pochtasi ${email}
+
+ Foydalanuvchining manzili ${address}   
+
+ Foydalanuvchining nomeri ${userNum}
+
+ Foydalanuvchining kasbi ${profession}
+
+ Foydalanuvchining sevimli mashg'uloti ${hobby}
+
+ Foydalanuvchining moshinasi ${car}
+
+
+`);
